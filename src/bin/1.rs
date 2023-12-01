2e64b6b3d822c357b530fb8a010fcaaf224dfc39
Ely <elyalon345@gmail.com>
2023-12-01 13:57:24 +0200

    day 1

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -3,11 +3,24 @@ fn calibration_sum(input: &str) -> u32 {
         .lines()
         .map(|line| {
             let mut digit_line = String::new();
+            let mut letter_sequence = Vec::new();
+
             for c in line.chars() {
                 if c.is_ascii_digit() {
-                    digit_line.push(c)
+                    digit_line.push(c);
+                    letter_sequence.clear();
+                } else {
+                    letter_sequence.push(c);
+                }
+
+                for combination in combinations(&letter_sequence) {
+                    match digitized(&combination) {
+                        Some(c) => digit_line.push(c),
+                        None => continue,
+                    }
                 }
             }
+
             return digit_line;
         })
         .collect::<Vec<_>>();
@@ -26,6 +39,33 @@ fn calibration_sum(input: &str) -> u32 {
     first_and_last_digit_lines
         .iter()
         .fold(0, |acc, x| acc + x.parse::<u32>().unwrap())
+}
+
+fn digitized(s: &str) -> Option<char> {
+    match s {
+        "one" => Some('1'),
+        "two" => Some('2'),
+        "three" => Some('3'),
+        "four" => Some('4'),
+        "five" => Some('5'),
+        "six" => Some('6'),
+        "seven" => Some('7'),
+        "eight" => Some('8'),
+        "nine" => Some('9'),
+        _ => None,
+    }
+}
+
+fn combinations(chars: &[char]) -> Vec<String> {
+    let mut combinations = Vec::new();
+
+    for start in 0..chars.len() {
+        for end in start + 1..=chars.len() {
+            combinations.push(chars[start..end].iter().collect());
+        }
+    }
+
+    combinations
 }
 
 fn main() {
